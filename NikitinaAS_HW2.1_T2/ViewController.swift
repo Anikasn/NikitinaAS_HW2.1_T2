f421b0aa8ee0b8e3c13e2f619c2b64a2edb2ddd4
Anikasn <anikasn21@gmail.com>
2021-07-07 20:10:16 +0300

    Set Start Button

--- NikitinaAS_HW2.1_T2/ViewController.swift
+++ NikitinaAS_HW2.1_T2/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = self.redLightView.frame.height / 2.0
+        redLightView.alpha = 0.3
+        
+        yellowLightView.layer.cornerRadius = self.yellowLightView.frame.height / 2.0
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = self.greenLightView.frame.height / 2.0
+        greenLightView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = self.startButton.frame.height / 2.0
     }
 
-
+    @IBAction func startButtonPressed() {
+        if redLightView.alpha == 1 {
+            startButton.setTitle("NEXT", for: .normal)
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        } else if yellowLightView.alpha == 1 {
+            startButton.setTitle("NEXT", for: .normal)
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        } else if greenLightView.alpha == 1 {
+            startButton.setTitle("NEXT", for: .normal)
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+        } else {
+            startButton.setTitle("START", for: .normal)
+            redLightView.alpha = 1
+        }
+    }
+    
 }
 
