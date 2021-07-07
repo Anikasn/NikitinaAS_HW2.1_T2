503ffeb679ae10ded31462a749c7bbe90977ee06
Anikasn <anikasn21@gmail.com>
2021-07-07 21:03:43 +0300

    Traffic light switch

--- NikitinaAS_HW2.1_T2/ViewController.swift
+++ NikitinaAS_HW2.1_T2/ViewController.swift
@@ -17,13 +17,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLightView.layer.cornerRadius = self.redLightView.frame.height / 2.0
+        redLightView.layer.cornerRadius = self.redLightView.frame.width / 2.0
         redLightView.alpha = 0.3
         
-        yellowLightView.layer.cornerRadius = self.yellowLightView.frame.height / 2.0
+        yellowLightView.layer.cornerRadius = self.yellowLightView.frame.width / 2.0
         yellowLightView.alpha = 0.3
         
-        greenLightView.layer.cornerRadius = self.greenLightView.frame.height / 2.0
+        greenLightView.layer.cornerRadius = self.greenLightView.frame.width / 2.0
         greenLightView.alpha = 0.3
         
         startButton.layer.cornerRadius = self.startButton.frame.height / 2.0
@@ -31,20 +31,17 @@ class ViewController: UIViewController {
 
     @IBAction func startButtonPressed() {
         if redLightView.alpha == 1 {
-            startButton.setTitle("NEXT", for: .normal)
             redLightView.alpha = 0.3
             yellowLightView.alpha = 1
         } else if yellowLightView.alpha == 1 {
-            startButton.setTitle("NEXT", for: .normal)
             yellowLightView.alpha = 0.3
             greenLightView.alpha = 1
         } else if greenLightView.alpha == 1 {
-            startButton.setTitle("NEXT", for: .normal)
             greenLightView.alpha = 0.3
             redLightView.alpha = 1
         } else {
-            startButton.setTitle("START", for: .normal)
             redLightView.alpha = 1
+            startButton.setTitle("NEXT", for: .normal)
         }
     }
     
