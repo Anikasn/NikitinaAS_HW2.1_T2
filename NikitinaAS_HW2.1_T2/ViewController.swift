3b10b72f815961e1f08c0b2f3ac375f8ae5b7771
Anikasn <anikasn21@gmail.com>
2021-07-07 12:18:06 +0300

    Initial Commit

---
+++ NikitinaAS_HW2.1_T2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NikitinaAS_HW2.1_T2
+//
+//  Created by Анна Никитина on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
